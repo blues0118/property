dc95e95a36f3b8c14904b838e727133b7b4ead32
blues0118 <blues0118@gmail.com>
2014-12-07 14:12:08 +0800

    20141207 zyh

--- src/java/net/ussoft/property/web/StandingBookController.java
+++ src/java/net/ussoft/property/web/StandingBookController.java
@@ -1,10 +1,13 @@
 package net.ussoft.property.web;
 
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.List;
+import java.util.UUID;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
@@ -19,8 +22,10 @@ import net.ussoft.property.base.BaseConstroller;
 import net.ussoft.property.model.PageBean;
 import net.ussoft.property.model.Project;
 import net.ussoft.property.model.Standingbookterm;
+import net.ussoft.property.model.Sys_account;
 import net.ussoft.property.service.IProjectService;
 import net.ussoft.property.service.IStandingBookService;
+import net.ussoft.property.util.MD5;
 
 @Controller
 @RequestMapping(value="standingbook")
@@ -56,15 +61,15 @@ public class StandingBookController extends BaseConstroller {
 	 * @param modelMap
 	 * @return
 	 */
-	@RequestMapping(value = "/list", method = RequestMethod.GET)
-	public void list(Integer page,String searchTxt,HttpServletResponse response) throws Exception {
+	@RequestMapping(value = "/list", method = RequestMethod.POST)
+	public void list(Integer page,String searchTxt,ModelMap modelMap, HttpServletResponse response) throws Exception {
 		
 		response.setContentType("text/xml;charset=UTF-8");
 		response.setCharacterEncoding("UTF-8");
 		PrintWriter out = response.getWriter();
 		
 		PageBean<Standingbookterm> pageBean = new PageBean<Standingbookterm>();
-		
+
 		//每页行数
 		Integer pageSize = 50;
 		
@@ -75,6 +80,7 @@ public class StandingBookController extends BaseConstroller {
 		pageBean.setOrderBy("termcode");
 		
 		Standingbookterm t = new Standingbookterm();
+		//t.setProjectid(modelMap.get("projeuctid").toString());
 		t.setProjectid("sdffadfa");
 		if (null != searchTxt && !"".equals(searchTxt)) {
 			t.setProjectid(searchTxt);
@@ -108,5 +114,48 @@ public class StandingBookController extends BaseConstroller {
 	public ModelAndView relist(String projeuctid,ModelMap modelMap) {
 		modelMap.put("projeuctid", projeuctid);
 		return new ModelAndView("/view/standingbook/list",modelMap);
+	}
+	
+	/**
+	 * 打开添加页面
+	 * @return
+	 */
+	@RequestMapping(value="/add",method=RequestMethod.GET)
+	public String add(ModelMap modelMap) {
+		return "/view/standingbook/add";
+	}
+
+	/**
+	 * 保存新帐户
+	 * @param standingbookterm
+	 * @param request
+	 * @param response
+	 * @throws IOException
+	 */
+	@RequestMapping(value="/save",method=RequestMethod.POST)
+	public void save(Standingbookterm standingbookterm,HttpServletRequest request,HttpServletResponse response) throws IOException {
+		
+		response.setContentType("text/xml;charset=UTF-8");
+		response.setCharacterEncoding("UTF-8");
+		PrintWriter out = response.getWriter();
+		
+		String result = "success";
+		if (standingbookterm == null ) {
+			result = "failure";
+			out.print(result);
+			return;
+		}
+		// id
+		standingbookterm.setId(UUID.randomUUID().toString());
+		// 项目id
+		standingbookterm.setProjectid("sdffadfa");  //TODO
+		standingbookterm = standingBookService.insert(standingbookterm);
+		
+		if (standingbookterm == null ) {
+			result = "failure";
+			out.print(result);
+			return;
+		}
+		out.print(result);
 	}
 }
